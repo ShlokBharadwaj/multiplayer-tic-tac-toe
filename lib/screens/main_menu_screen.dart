58a35e58259393da950bf21ef1f0d501c68da2f0
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-02 17:00:38 +0530

    Add: Button to Create and Join room in Main Menu file

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/widgets/custom_button.dart';
 
 class MainMenuScreen extends StatelessWidget {
   const MainMenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Main menu'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomButton(
+            onTap: () {},
+            text: 'Create Room',
+          ),
+          const SizedBox(height: 20),
+          CustomButton(
+            onTap: () {},
+            text: 'Join Room',
+          ),
+        ],
       ),
     );
   }

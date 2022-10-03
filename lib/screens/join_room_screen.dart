d6e4f1578a7835cdebdc5997edb55f7e7208285d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-03 17:00:32 +0530

    Add: Join room Custom text, textfield and button

--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/responsive/responsive.dart';
+import 'package:tic_tac_toe/widgets/custom_button.dart';
+import 'package:tic_tac_toe/widgets/custom_text.dart';
+import 'package:tic_tac_toe/widgets/custom_textfield.dart';
 
 class JoinRoomScreen extends StatefulWidget {
   static String routeName = '/join-room';
@@ -10,8 +14,63 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _joinRoomGamingNameController =
+      TextEditingController();
+  final TextEditingController _gameIdController = TextEditingController();
+
+  @override
+  void dispose() {
+    _joinRoomGamingNameController.dispose();
+    _gameIdController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 30.0,
+                    color: Colors.lightBlue,
+                    offset: Offset(5.0, 5.0),
+                  )
+                ],
+                text: 'Join Room',
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              CustomTextField(
+                controller: _joinRoomGamingNameController,
+                hintText: 'Enter your Gaming name',
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              CustomTextField(
+                controller: _gameIdController,
+                hintText: 'Enter Game ID',
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              CustomButton(onTap: () {}, text: 'Join'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

cc8075dc32fd3a0293df47fc720036abc64be3eb
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-03 06:48:20 +0530

    Add: Routes for Create and Join room screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/screens/create_room_screen.dart';
+import 'package:tic_tac_toe/screens/join_room_screen.dart';
 import 'package:tic_tac_toe/screens/main_menu_screen.dart';
 import 'package:tic_tac_toe/utils/colors.dart';
 
@@ -18,6 +20,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+        JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+        CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
       },
       home: const MainMenuScreen(),
     );

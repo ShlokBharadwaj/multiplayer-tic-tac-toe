0adbe91444e29edddb998408ccdc8bcbce139040
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-05 19:41:03 +0530

    Add: Provider for Roomdata

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/provider/room_data_provider.dart';
+import 'package:tic_tac_toe/widgets/waiting_lobby.dart';
 
 class GameScreen extends StatefulWidget {
   static String routeName = '/game';
@@ -11,7 +14,16 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
-    // Provider.of<RoomDataProvider>(context).updateRoomData({});
-    return Scaffold();
+    RoomDataProvider roomDataProvider = Provider.of<RoomDataProvider>(context);
+
+    return Scaffold(
+      body: roomDataProvider.roomData['isJoin']
+          ? const WaitingLobby()
+          : Center(
+              child: Text(
+                Provider.of<RoomDataProvider>(context).roomData.toString(),
+              ),
+            ),
+    );
   }
 }

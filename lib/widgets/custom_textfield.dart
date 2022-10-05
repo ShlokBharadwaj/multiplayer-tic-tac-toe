1b05e0e3580fff3bff9dd7e453afb8a856b0ba4e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-05 19:40:32 +0530

    Update: Center align Text inside Textfield

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
+  final bool isReadOnly;
   const CustomTextField({
     super.key,
     required this.controller,
     required this.hintText,
+    this.isReadOnly = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: TextField(
+        textAlign: TextAlign.center,
+        readOnly: isReadOnly,
         controller: controller,
         decoration: InputDecoration(
           hintText: hintText,

c7b6e820106fcf80840991f7a041980977c7980a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-05 06:39:09 +0530

    Add: Getter for Player1 & Player2

--- lib/provider/room_data_provider.dart
+++ lib/provider/room_data_provider.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/models/players.dart';
 
 class RoomDataProvider extends ChangeNotifier {
   Map<String, dynamic> _roomData = {};
+
+  Players _player1 = Players(
+    gamingName: '',
+    socketId: '',
+    points: 0.0,
+    playerType: 'X',
+  );
+
+  Players _player2 = Players(
+    gamingName: '',
+    socketId: '',
+    points: 0.0,
+    playerType: 'O',
+  );
+
   Map<String, dynamic> get roomData => _roomData;
+
+  Players get player1 => _player1;
+  Players get player2 => _player2;
 
   void updateRoomData(Map<String, dynamic> roomData) {
     _roomData = roomData;

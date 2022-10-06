689698d98adba723356a5e2d7f5d440a9a6121de
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-06 07:04:30 +0530

    Add: Responsive TicTacToe game board

--- lib/widgets/tictactoe_board.dart
+++ lib/widgets/tictactoe_board.dart
@@ -10,36 +10,43 @@ class TicTacToe extends StatefulWidget {
 class _TicTacToeState extends State<TicTacToe> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 9,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
+    final size = MediaQuery.of(context).size;
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: size.height * 0.7,
+        maxWidth: 500,
       ),
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          decoration: BoxDecoration(
-            border: Border.all(),
-            color: Colors.white24,
-          ),
-          child: const Center(
-            child: Text(
-              'X',
-              style: TextStyle(
-                fontSize: 100,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                shadows: [
-                  Shadow(
-                    blurRadius: 10.0,
-                    color: Colors.lightBlue,
-                    offset: Offset(5.0, 5.0),
-                  ),
-                ],
+      child: GridView.builder(
+        itemCount: 9,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              color: Colors.white24,
+            ),
+            child: const Center(
+              child: Text(
+                'X',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.lightBlue,
+                      offset: Offset(5.0, 5.0),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }

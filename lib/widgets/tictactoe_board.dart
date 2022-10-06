32b25723ec035c819d13ab570582ee6fb493ebef
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-06 07:25:18 +0530

    Add: Gesture detector for click and update the board

--- lib/widgets/tictactoe_board.dart
+++ lib/widgets/tictactoe_board.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/provider/room_data_provider.dart';
+import 'package:tic_tac_toe/resources/socket_methods.dart';
 
 class TicTacToe extends StatefulWidget {
   const TicTacToe({super.key});
@@ -8,9 +11,20 @@ class TicTacToe extends StatefulWidget {
 }
 
 class _TicTacToeState extends State<TicTacToe> {
+  final SocketMethods _socketMethods = SocketMethods();
+
+  void tapped(int index, RoomDataProvider roomDataProvider) {
+    _socketMethods.tapOnBox(
+      index,
+      roomDataProvider.roomData['_id'],
+      roomDataProvider.displayElements,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    RoomDataProvider roomDataProvider = Provider.of<RoomDataProvider>(context);
     return ConstrainedBox(
       constraints: BoxConstraints(
         maxHeight: size.height * 0.7,
@@ -22,25 +36,31 @@ class _TicTacToeState extends State<TicTacToe> {
           crossAxisCount: 3,
         ),
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(),
-              color: Colors.white24,
+          return GestureDetector(
+            onTap: () => tapped(
+              index,
+              roomDataProvider,
             ),
-            child: const Center(
-              child: Text(
-                'X',
-                style: TextStyle(
-                  fontSize: 100,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10.0,
-                      color: Colors.lightBlue,
-                      offset: Offset(5.0, 5.0),
-                    ),
-                  ],
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+                color: Colors.white24,
+              ),
+              child: const Center(
+                child: Text(
+                  'X',
+                  style: TextStyle(
+                    fontSize: 100,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.lightBlue,
+                        offset: Offset(5.0, 5.0),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),

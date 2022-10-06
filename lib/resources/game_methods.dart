f0d2d2be55e2b967c1895a3c81605ce5209b9477
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-10-06 10:22:26 +0530

    Add: Method to check winner by row

---
+++ lib/resources/game_methods.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:tic_tac_toe/provider/room_data_provider.dart';
+
+class GameMethods {
+  void checkWinner(BuildContext context, Socket socketClient) {
+    RoomDataProvider roomDataProvider =
+        Provider.of<RoomDataProvider>(context, listen: false);
+
+    String winner = '';
+
+    if (roomDataProvider.displayElements[0] ==
+            roomDataProvider.displayElements[1] &&
+        roomDataProvider.displayElements[1] ==
+            roomDataProvider.displayElements[2] &&
+        roomDataProvider.displayElements[0] != '') {
+      winner = roomDataProvider.displayElements[0];
+    }
+
+    if (roomDataProvider.displayElements[3] ==
+            roomDataProvider.displayElements[4] &&
+        roomDataProvider.displayElements[4] ==
+            roomDataProvider.displayElements[5] &&
+        roomDataProvider.displayElements[3] != '') {
+      winner = roomDataProvider.displayElements[3];
+    }
+
+    if (roomDataProvider.displayElements[6] ==
+            roomDataProvider.displayElements[7] &&
+        roomDataProvider.displayElements[7] ==
+            roomDataProvider.displayElements[8] &&
+        roomDataProvider.displayElements[6] != '') {
+      winner = roomDataProvider.displayElements[6];
+    }
+  }
+}
